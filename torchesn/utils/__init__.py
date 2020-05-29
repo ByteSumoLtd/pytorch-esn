447372ce5835a03a7d06c90e64ab270c887ead24
Andrew <andrew@bytesumo.com>
2020-05-29 09:40:32 +0100

    making commandline ESN tool, move it to utills

--- torchesn/utils/__init__.py
+++ torchesn/utils/__init__.py
@@ -1,3 +1,4 @@
 from .utilities import prepare_target, washout_tensor
+from .fn_mackey_glass import executeESN
 
-__all__ = ['prepare_target', 'washout_tensor']
+__all__ = ['prepare_target', 'washout_tensor', 'executeESN']
