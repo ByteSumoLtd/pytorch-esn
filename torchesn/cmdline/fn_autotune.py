d3447777745d4ce0558ba4d997d6fbb261244efa
Andrew <andrew@bytesumo.com>
2020-06-03 19:23:32 +0100

    minor corrections to get min_layers setting to work

--- torchesn/cmdline/fn_autotune.py
+++ torchesn/cmdline/fn_autotune.py
@@ -33,7 +33,7 @@ import click
 @click.option("--output_size", default=1, help="size input size data, a univariate timeseries is 1: default 1")     
 @click.option("--batch_first", default=False, help="If True the input and output tensors are provided as (batch, seq, feature). Default: False")
 @click.option("--max_layers", default=1, type=int, help="the maximum number of layers use in our DeepESN search. default: 1")
-@click.option("--min_layers", defatult=1, type=int, help="the minimum number of layers to use in our DeepESN search. default: 1")
+@click.option("--min_layers", default=1, type=int, help="the minimum number of layers to use in our DeepESN search. default: 1")
 @click.option("--hidden_size_low", default=500, help="lower bound of the hidden reservoir to search: default 500")
 @click.option("--hidden_size_high", default=1000, help="upper bound of the hidden size to search. Default 2000")
 
@@ -43,7 +43,7 @@ import click
 @click.option("--header", type = click.BOOL, default=False, help="a switch to print the header record to interpret the results. Default: False")
 
 
-def tuneESN(dataset, input_size, output_size, batch_first, max_layers, hidden_size_low, hidden_size_high, population, generations, header, worker_pool):
+def tuneESN(dataset, input_size, output_size, batch_first, max_layers, min_layers, hidden_size_low, hidden_size_high, population, generations, header, worker_pool):
      #     Above we created just enough commandline parameters to run the autotuning. 
      #     I have set smart search defaults for all the ESN search hyperparameters.
      #     Due to cost implications, I'm leaving in hidden_low, hidden_high, population size, and number of generations for you to override.
