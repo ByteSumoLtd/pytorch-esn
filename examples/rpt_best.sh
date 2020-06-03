35d9de0406ce29e822bf4916e81949ed03eacbfa
Andrew <andrew@bytesumo.com>
2020-06-03 11:06:09 +0100

    moved log file to logs dir

--- examples/rpt_best.sh
+++ examples/rpt_best.sh
@@ -1,1 +1,1 @@
-cat runlog.csv | head -1 | sed 's/,/\t/g' > tmp ; cat runlog.csv | grep -v train | sort -g -t, -k3,3  | head -30 | sed 's/,/\t/g' >>tmp ; cat tmp | column -t; rm tmp
+cat logs/runlog.csv | head -1 | sed 's/,/\t/g' > tmp ; cat logs/runlog.csv | grep -v train | sort -g -t, -k3,3  | head -30 | sed 's/,/\t/g' >>tmp ; cat tmp | column -t; rm tmp
